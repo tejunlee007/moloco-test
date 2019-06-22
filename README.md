07d20b108e21798245fb900eacd3eb9997fb2d1b
Taejun Lee <tejunlee007@gmail.com>
2019-06-22 13:34:20 +0900

    ADD: make a q1.py for question 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # moloco-test
+this is a coding test work for moloco
+
+## q1.py
+this file returns true/false if strings are the same when one character is removed from one of input strings  
---
+++ q1.py
@@ -0,0 +1,40 @@
+def equalsWhenOneCharRemoved(str1, str2):
+    if len(str1) == len(str2):
+        return False
+    if len(str1) - len(str2) > 1:
+        return False
+
+    long_str = str1 if len(str1) > len(str2) else str2
+    short_str = str1 if len(str1) < len(str2) else str2
+
+    for i, c in enumerate(long_str):
+        if len(short_str) == i:
+            return False
+        if long_str[i] != short_str[i]:
+            temp_long_str = long_str
+            if i == 0:
+                temp_long_str = temp_long_str[i:]
+            else:
+                temp_long_str = temp_long_str[:i-1] + temp_long_str[i:]
+            if temp_long_str == short_str:
+                return True
+
+    return False
+
+
+# tests
+# case 1
+str1 = '{}{}{}{}'.format('a' * 6, 'b' * 5, 'c' * 4, 'd' * 3)
+str2 = '{}{}{}{}'.format('a' * 6, 'b' * 6, 'c' * 4, 'd' * 3)
+print(equalsWhenOneCharRemoved(str1, str2))
+
+# case 2
+str1 = '{}{}{}{}'.format('a' * 600000, 'b' * 500000, 'c' * 400000, 'd' * 300000)
+str2 = '{}{}{}{}'.format('a' * 600000, 'b' * 500001, 'c' * 400000, 'd' * 300000)
+print(equalsWhenOneCharRemoved(str1, str2))
+
+# case 3
+str1 = '{}{}{}{}'.format('a' * 600000, 'b' * 500000, 'c' * 400002, 'd' * 300000)
+str2 = '{}{}{}{}'.format('a' * 600000, 'b' * 500000, 'c' * 400000, 'd' * 300000)
+print(equalsWhenOneCharRemoved(str1, str2))
+
